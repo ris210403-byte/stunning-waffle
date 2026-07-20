fc92529734d409a1f13c8f8178de1eed5df8ccf6
ris210403-byte <ris210403@gmail.com>
2026-07-20 19:20:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kaki-jalan-v12';
+const CACHE_NAME = 'kaki-jalan-v13';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './splash-logo.png'];
 
 self.addEventListener('install', (event) => {
